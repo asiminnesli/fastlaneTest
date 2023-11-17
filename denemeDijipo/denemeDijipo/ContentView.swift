8c232912e9d9b7eab75512637ba1152fa113e793
asiminnesli <asimmurat17@gmail.com>
2023-11-17 21:25:27 +0300

    init

---
+++ denemeDijipo/denemeDijipo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  denemeDijipo
+//
+//  Created by Asım YILMAZ on 17.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ denemeDijipo/denemeDijipo/denemeDijipoApp.swift
@@ -0,0 +1,17 @@
+//
+//  denemeDijipoApp.swift
+//  denemeDijipo
+//
+//  Created by Asım YILMAZ on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct denemeDijipoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
